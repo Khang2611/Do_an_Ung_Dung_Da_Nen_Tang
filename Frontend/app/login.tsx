6d78aed75d4013013324be28adb4450731872277
HuceLongK <vulongcl158@gmail.com>
2026-04-22 23:10:27 +0700

    Điều hướng và UI chính của app.

---
+++ Frontend/app/login.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import LoginModule from "@/components/auth/login";
+
+export default function LoginScreen() {
+  return <LoginModule />;
+}
---
+++ Frontend/app/register.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import RegisterScreen from "@/components/auth/register";
+
+export default function RegisterRoute() {
+  return <RegisterScreen />;
+}
